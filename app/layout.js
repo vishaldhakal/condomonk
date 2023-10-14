4dac84665d5900cd40d426e0cf8b923344cbc0ac
vishaldhakal <vishaldhakal96@gmail.com>
2023-10-14 19:53:16 +0545

    AdV

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import "./icons.css";
 import "react-quill/dist/quill.snow.css";
@@ -6,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Montserrat } from "next/font/google";
 import Providers from "./providers";
+import Head from "next/head";
 
 const montserrat = Montserrat({ subsets: ["cyrillic"] });
 
@@ -74,17 +74,23 @@ async function getCities() {
 export default async function RootLayout({ children }) {
   let cities = await getCities();
   return (
-    <html lang="en">
+    <>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+      />
       <body className={montserrat.className}>
         <Navbar cities={cities}></Navbar>
         <Providers>{children}</Providers>
         <Footer cities={cities}></Footer>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-          crossorigin="anonymous"
-        ></script>
       </body>
-    </html>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"
+      ></script>
+    </>
   );
 }
