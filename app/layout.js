7c97ca65440e7fcfd0f7befb6c5b1e1c94fa61e0
vishaldhakal <vishaldhakal96@gmail.com>
2023-10-14 19:34:30 +0545

    adVF

--- app/layout.js
+++ app/layout.js
@@ -80,8 +80,8 @@ export default async function RootLayout({ children }) {
         <Providers>{children}</Providers>
         <Footer cities={cities}></Footer>
         <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
           crossorigin="anonymous"
         ></script>
       </body>
