7771e30b24dcda668ac955b4815302203e3fc3d3
vishaldhakal <vishaldhakal96@gmail.com>
2023-10-14 20:15:58 +0545

    AdV

--- app/layout.js
+++ app/layout.js
@@ -75,12 +75,14 @@ export default async function RootLayout({ children }) {
   let cities = await getCities();
   return (
     <html lang="en">
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossorigin="anonymous"
-      />
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous"
+        />
+      </head>
       <body className={montserrat.className}>
         <Navbar cities={cities}></Navbar>
         <Providers>{children}</Providers>
