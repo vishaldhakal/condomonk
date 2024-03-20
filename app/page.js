c1949ed63aae00b0c2bf8ddda6ff806666dc78d4
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-03-20 20:43:37 +0545

    css

--- app/page.js
+++ app/page.js
@@ -80,7 +80,7 @@ export default async function Home(props) {
             Canada's leading  
               <div className="relative inline-flex sm-center">
                 <span className="absolute inset-x-0 bottom-0 border-b-[30px] border-[#4ADE80]"></span>
-                <h1 className="relative text-3xl font-bold text-black sm:text-6xl lg:text-7xl ">pre construction.</h1>
+                <h1 className="relative text-3xl font-bold text-black sm:text-6xl lg:text-7xl ">pre construction</h1>
                
               </div>
 
