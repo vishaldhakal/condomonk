b28ec56d7132b64039ae22b5d1c3e7eae73981da
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-04-20 17:41:06 +0545

    lazy loading

--- components/Gallery.js
+++ components/Gallery.js
@@ -29,27 +29,27 @@ export default function Gallery(props) {
 
   return (
     <div className="my-3 grid-cont">
-    <LightGallery onInit={onInit} speed={500} plugins={[lgThumbnail, lgZoom]}>
-      {newImages(props.images)
-        ?.slice(0, 7)
-        .map((image, no) => (
-          <Link
-            href={`${image.image}`}
-            className={`position-relative g-item grid-item${parseInt(no + 1)}`} /* Adjusted class assignment */
-            key={no}
-          >
-            <img
-              alt={`${props.project_name} located at ${
-                props.project_address
-              } image ${no + 1}`}
-              className="img-fluid w-100 h-100 rounded-mine"
-              src={`${image.image}`}
-            />
-          </Link>
-        ))}
-    </LightGallery>
-  </div>
-  
-    
+      <LightGallery onInit={onInit} speed={500} plugins={[lgThumbnail, lgZoom]}>
+        {newImages(props.images)
+          ?.slice(0, 7)
+          .map((image, no) => (
+            <Link
+              href={`${image.image}`}
+              className={`position-relative g-item grid-item${parseInt(
+                no + 1
+              )}`} /* Adjusted class assignment */
+              key={no}
+            >
+              <img
+                alt={`${props.project_name} located at ${
+                  props.project_address
+                } image ${no + 1}`}
+                className="img-fluid w-100 h-100 rounded-mine lazy"
+                src={`${image.image}`}
+              />
+            </Link>
+          ))}
+      </LightGallery>
+    </div>
   );
 }
