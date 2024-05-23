709765295cb90397b0aa7df3319d4a1f5b64745d
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-05-23 12:51:31 +0545

    next chunk

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output.filename = "static/chunks/[name].[contenthash:8].js";
+      config.output.publicPath = "/_next/";
+    }
+    return config;
+  },
+};
