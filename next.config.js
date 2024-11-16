0936f5d71a7765ff4463161517c055279bb886e1
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-11-16 22:35:53 +0545

    dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other necessary configurations
+};
 
 module.exports = {
   images: {
