b3007b3a90fddb5d167a0b221e9168a212c2e0af
Naseeb <40313494+naseeb0@users.noreply.github.com>
2024-11-17 11:35:34 +0545

    Asd

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other necessary configurations
+};
+
+module.exports = {
+  images: {
+    domains: ["api.condomonk.ca"],
+  },
+};
