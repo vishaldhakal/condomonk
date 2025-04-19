9cb687846eee9cc2e0fbb759d0fe711f32035458
Udbhav Ghimire <ghimireudbhav@gmail.com>
2025-04-19 09:52:04 +0545

    chnages

--- next.config.js
+++ next.config.js
@@ -3,17 +3,28 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   // Add any other necessary configurations
-  remotePatterns: [
-    {
-      hostname: "api.condomonk.ca",
-    },
-    {
-      hostname: "trreb-image.ampre.ca",
-    },
-    {
-      hostname: "api.toassign.com",
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.condomonk.ca",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "trreb-image.ampre.ca",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.toassign.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
