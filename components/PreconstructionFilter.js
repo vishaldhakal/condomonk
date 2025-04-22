ea07b61daeb3fc33bce0b763a1685117e29b90e5
Udbhav Ghimire <ghimireudbhav@gmail.com>
2025-04-22 23:56:33 +0545

    fd

--- components/PreconstructionFilter.js
+++ components/PreconstructionFilter.js
@@ -151,7 +151,7 @@ const PreconstructionFilter = ({ cityName, citySlug }) => {
                 <Link
                   key={option.value}
                   href={getLinkPath(option, "property")}
-                  className="block px-4 py-2 text-sm hover:bg-gray-100 text-gray-700"
+                  className="block px-4 py-2 text-sm bg-gray-100 text-gray-700"
                 >
                   {option.label}
                 </Link>
