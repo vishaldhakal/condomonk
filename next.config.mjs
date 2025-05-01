0d9b00faf7ba9cd029598192e88d82185a84ae07
Udbhav Ghimire <ghimireudbhav@gmail.com>
2025-05-01 17:56:01 +0545

    cvhjk

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.condomonk.ca",
+      },
+      {
+        protocol: "https",
+        hostname: "api.toassign.com",
+      },
+      {
+        protocol: "https",
+        hostname: "trreb-image.ampre.ca",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
