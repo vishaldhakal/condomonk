61f8c4726680c7d26826fa2a8cd9bb2bcd54d941
Udbhav Ghimire <ghimireudbhav@gmail.com>
2025-05-07 23:35:56 +0545

    not found js

---
+++ app/[city]/[slug]/not-found.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import HomeSearch from "@/components/HomeSearch";
+import RealEstateLinks from "@/components/RealEstateLinks";
+
+async function getCities() {
+  const res = await fetch("https://api.condomonk.ca/api/all-city", {
+    next: { revalidate: 10 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function NotFound() {
+  let cities = await getCities();
+
+  return (
+    <>
+      <div className="py-20"></div>
+      <div>
+        <div className="flex flex-col items-center justify-center">
+          <img src="/404.png" alt="condomonk" className="w-32 h-32" />
+          <h4 className="text-center font-bold mb-0">
+            <span className="text-red-600 text-6xl">Oops!</span> <br />
+            <span className="text-4xl">
+              Sorry, we couldn't find the listing you're looking for.
+            </span>
+          </h4>
+
+          <div className="pb-1 pt-20 px-3 w-full max-w-4xl mx-auto">
+            {/* Search Component */}
+            <HomeSearch />
+          </div>
+        </div>
+      </div>
+      <div className="py-20 max-w-7xl mx-auto">
+        <RealEstateLinks />
+      </div>
+    </>
+  );
+}
