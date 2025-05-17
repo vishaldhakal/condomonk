79d7f295dd20910a9d7f3c679198c72f7484fdc4
Udbhav Ghimire <ghimireudbhav@gmail.com>
2025-05-17 08:04:42 +0545

    boder

--- app/[city]/page.js
+++ app/[city]/page.js
@@ -515,7 +515,7 @@ export default async function CityPage({ params }) {
             <div className="prose max-w-none px-3 mt-8">
               <div
                 dangerouslySetInnerHTML={{ __html: data.city.details }}
-                className="prose prose-sm md:prose-base lg:prose-lg max-w-none prose-headings:font-bold prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-p:text-gray-600 prose-a:text-blue-600 hover:prose-a:text-blue-800 prose-img:rounded-lg rich-text"
+                className="prose prose-sm md:prose-base lg:prose-lg max-w-none prose-headings:font-bold prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-p:text-gray-600 prose-a:text-blue-600 hover:prose-a:text-blue-800 prose-img:rounded-lg [&_table]:!border [&_table]:!border-collapse [&_table]:!border-solid [&_table]:!border-black [&_th]:!border [&_th]:!border-solid [&_th]:!border-black [&_th]:!p-2 [&_td]:!border [&_td]:!border-solid [&_td]:!border-black [&_td]:!p-2 [&_tr]:!border [&_tr]:!border-solid [&_tr]:!border-black rich-text"
               />
             </div>
             <p className="text-sm text-gray-500 text-center mt-8">
